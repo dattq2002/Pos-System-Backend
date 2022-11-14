74ff98ea8b48b920f20fb40b261868810fed856e
Đặng Xuân Đạt <68004908+dangxuandat@users.noreply.github.com>
2022-11-14 09:00:51 +0700

    Set up unit of work (#4)
    * Adjust gitignore
    
    * Add Models and DYNAMIC Unit Of Work
    
    * Set up base controller
    
    * Add account controller
    
    * Add OrderController
    
    Co-authored-by: dat-dang <dat.dang@billidentity.com>

---
+++ Pos-System-Backend/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pos_System_Backend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class OrderController : ControllerBase
+	{
+	}
+}
