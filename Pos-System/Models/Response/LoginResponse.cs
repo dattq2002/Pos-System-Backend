f582bdcc08bb2d3156f1c1058fe285c86c5a9d1d
Đặng Xuân Đạt <68004908+dangxuandat@users.noreply.github.com>
2022-12-03 13:35:08 +0700

    Pst 53 login account (#11)
    * Adjust Role Enum
    
    * pst-53 add login
    
    * pst-53 Add Authorization
    
    * pst-53 Add role in response

--- Pos-System/Models/Response/LoginResponse.cs
+++ Pos-System/Models/Response/LoginResponse.cs
@@ -8,5 +8,6 @@ public class LoginResponse
 	public Guid Id { get; set; }
 	public string Username { get; set; }
 	public string Name { get; set; }
+    public RoleEnum Role { get; set; }
 	public AccountStatus Status { get; set; }
 }
