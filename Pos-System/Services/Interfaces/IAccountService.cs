d1de3c1c68ac10cc8a0797d7fb9724f0c45acf20
Dang Xuan Dat <dangxuandat0@gmail.com>
2023-01-05 22:28:03 +0700

    PST-18 Add filter to search

--- Pos-System/Services/Interfaces/IAccountService.cs
+++ Pos-System/Services/Interfaces/IAccountService.cs
@@ -1,4 +1,5 @@
-﻿using Pos_System.API.Payload.Request;
+﻿using Pos_System.API.Enums;
+using Pos_System.API.Payload.Request;
 using Pos_System.API.Payload.Request.Brands;
 using Pos_System.API.Payload.Response;
 using Pos_System.Domain.Models;
@@ -12,6 +13,6 @@ namespace Pos_System.API.Services.Interfaces
 
 		Task<CreateNewBrandAccountResponse> CreateNewBrandAccount(CreateNewBrandAccountRequest createNewBrandAccountRequest);
 
-		Task<IPaginate<GetAccountResponse>> GetBrandAccounts(Guid brandId, int page, int size);
+		Task<IPaginate<GetAccountResponse>> GetBrandAccounts(Guid brandId, string? searchUsername, RoleEnum role, int page, int size);
 	}
 }
