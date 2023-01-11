a495cb8c1f3a8fe714f74ca9a12ac04874c34f17
Đặng Xuân Đạt <68004908+dangxuandat@users.noreply.github.com>
2023-01-11 20:15:07 +0700

    Pst 18 (#29)
    * PST-18 Add get brand accounts api
    
    * PST-18 Add filter to search
    
    * PST-18 Add number of stores
    
    * Remove code

--- Pos-System/Payload/Response/Brands/GetBrandResponse.cs
+++ Pos-System/Payload/Response/Brands/GetBrandResponse.cs
@@ -4,22 +4,24 @@ namespace Pos_System.API.Payload.Response.Brands;
 
 public class GetBrandResponse
 {
-	public Guid Id { get; set; }
-	public string Name { get; set; }
-	public string? Email { get; set; }
-	public string? Address { get; set; }
-	public string? Phone { get; set; }
-	public string? PicUrl { get; set; }
-	public BrandStatus Status { get; set; }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+    public string? PicUrl { get; set; }
+    public BrandStatus Status { get; set; }
+    public int NumberOfStores { get; set; } = 0;
 
-	public GetBrandResponse(Guid id, string name, string email, string address, string phone, string picUrl, BrandStatus status)
-	{
-		Id = id;
-		Name = name;
-		Email = email;
-		Address = address;
-		Phone = phone;
-		PicUrl = picUrl;
-		Status = status;
-	}
+    public GetBrandResponse(Guid id, string name, string email, string address, string phone, string picUrl, BrandStatus status, int numberOfStores)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Address = address;
+        Phone = phone;
+        PicUrl = picUrl;
+        Status = status;
+        NumberOfStores = numberOfStores;
+    }
 }
