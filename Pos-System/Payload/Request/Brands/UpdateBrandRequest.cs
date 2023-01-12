351645c5a754c01acc72a4c30ed2e308ad98cec9
Đặng Xuân Đạt <68004908+dangxuandat@users.noreply.github.com>
2023-01-12 22:03:18 +0700

    PST-63 Add update brand information api (#32)

---
+++ Pos-System/Payload/Request/Brands/UpdateBrandRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pos_System.API.Payload.Request.Brands;
+
+public class UpdateBrandRequest
+{
+	public string? Name { get; set; }
+	[EmailAddress(ErrorMessage = "Email sai format")]
+	public string? Email { get; set; }
+	public string? Address { get; set; }
+	[Phone(ErrorMessage = "Phone sai format")]
+	public string? Phone { get; set; }
+	public string? PicUrl { get; set; }
+
+	public UpdateBrandRequest()
+	{
+	}
+
+	public void TrimString()
+	{
+		Name = Name?.Trim();
+		Email = Email?.Trim();
+		Address = Address?.Trim();
+		Phone = Phone?.Trim();
+		PicUrl = PicUrl?.Trim();
+	}
+}
