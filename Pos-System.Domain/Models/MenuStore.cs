759a49203cccd59867b078ec1ca0666a12d697fb
Dang Xuan Dat <dangxuandat0@gmail.com>
2023-02-20 22:06:00 +0700

    Migrate database

--- Pos-System.Domain/Models/MenuStore.cs
+++ Pos-System.Domain/Models/MenuStore.cs
@@ -8,11 +8,6 @@ namespace Pos_System.Domain.Models
         public Guid Id { get; set; }
         public Guid MenuId { get; set; }
         public Guid StoreId { get; set; }
-        public int? Priority { get; set; }
-        public int? DateFilter { get; set; }
-        public int? StartTime { get; set; }
-        public string Status { get; set; } = null!;
-        public int? EndTime { get; set; }
 
         public virtual Menu Menu { get; set; } = null!;
         public virtual Store Store { get; set; } = null!;
