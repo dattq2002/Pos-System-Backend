d802ec4c3e47d91e0ec230f839add5707d556c23
quandangdong <dongquan1212@gmail.com>
2023-02-28 18:24:19 +0700

    PST-148: As a brand admin, I want to view list product in menu

--- Pos-System/Services/Implements/MenuService.cs
+++ Pos-System/Services/Implements/MenuService.cs
@@ -4,6 +4,7 @@ using Pos_System.API.Constants;
 using Pos_System.API.Enums;
 using Pos_System.API.Payload.Request.Menus;
 using Pos_System.API.Payload.Response.Menus;
+using Pos_System.API.Payload.Response.Products;
 using Pos_System.API.Services.Interfaces;
 using Pos_System.API.Utils;
 using Pos_System.Domain.Models;
@@ -23,93 +24,93 @@ namespace Pos_System.API.Services.Implements
             Guid brandId = Guid.Parse(GetBrandIdFromJwt());
             if (brandId == Guid.Empty) throw new BadHttpRequestException(MessageConstant.Brand.EmptyBrandIdMessage);
             Brand brand = await _unitOfWork.GetRepository<Brand>()
-	            .SingleOrDefaultAsync(predicate: x => x.Id.Equals(brandId));
+                .SingleOrDefaultAsync(predicate: x => x.Id.Equals(brandId));
             if (brand == null) throw new BadHttpRequestException(MessageConstant.Brand.BrandNotFoundMessage);
-	        _logger.LogInformation($"Create new Menu with menu code: {createNewMenuRequest.Code}");
-	        Menu newMenu = new Menu()
-	        {
-		        Id = Guid.NewGuid(),
-		        Code = createNewMenuRequest.Code.Trim(),
-		        DateFilter = createNewMenuRequest.DateFilter,
-				Priority = createNewMenuRequest.Priority,
-		        StartTime = createNewMenuRequest.StartTime,
-		        EndTime = createNewMenuRequest.EndTime,
-		        BrandId = brand.Id,
-		        CreatedBy = GetUsernameFromJwt(),
-		        CreatedAt = DateTime.UtcNow,
-		        Status = EnumUtil.GetDescriptionFromEnum(MenuStatus.Deactivate)
-	        };
-			if (createNewMenuRequest.IsBaseMenu)
-	        {
-		        newMenu.Priority = 0; //Default priority of base menu is 0
-				newMenu.MenuStores = new List<MenuStore>();
-		        IEnumerable<Guid> storesInBrand = await _unitOfWork.GetRepository<Store>()
-			        .GetListAsync(
+            _logger.LogInformation($"Create new Menu with menu code: {createNewMenuRequest.Code}");
+            Menu newMenu = new Menu()
+            {
+                Id = Guid.NewGuid(),
+                Code = createNewMenuRequest.Code.Trim(),
+                DateFilter = createNewMenuRequest.DateFilter,
+                Priority = createNewMenuRequest.Priority,
+                StartTime = createNewMenuRequest.StartTime,
+                EndTime = createNewMenuRequest.EndTime,
+                BrandId = brand.Id,
+                CreatedBy = GetUsernameFromJwt(),
+                CreatedAt = DateTime.UtcNow,
+                Status = EnumUtil.GetDescriptionFromEnum(MenuStatus.Deactivate)
+            };
+            if (createNewMenuRequest.IsBaseMenu)
+            {
+                newMenu.Priority = 0; //Default priority of base menu is 0
+                newMenu.MenuStores = new List<MenuStore>();
+                IEnumerable<Guid> storesInBrand = await _unitOfWork.GetRepository<Store>()
+                    .GetListAsync(
                         selector: x => x.Id,
-				        predicate: x => x.BrandId.Equals(brand.Id));
-		        foreach (var storeGuid in storesInBrand)
-		        {
-			        newMenu.MenuStores.Add(new MenuStore()
-			        {
-				        Id = Guid.NewGuid(),
-				        MenuId = newMenu.Id,
-				        StoreId = storeGuid
-			        });
-		        }
-	        } //Create a base menu for brand that will apply for all stores
-			await _unitOfWork.GetRepository<Menu>().InsertAsync(newMenu);
-			bool isSuccessfully = await _unitOfWork.CommitAsync() > 0;
-			if (isSuccessfully) return newMenu.Id;
-			return Guid.Empty;
+                        predicate: x => x.BrandId.Equals(brand.Id));
+                foreach (var storeGuid in storesInBrand)
+                {
+                    newMenu.MenuStores.Add(new MenuStore()
+                    {
+                        Id = Guid.NewGuid(),
+                        MenuId = newMenu.Id,
+                        StoreId = storeGuid
+                    });
+                }
+            } //Create a base menu for brand that will apply for all stores
+            await _unitOfWork.GetRepository<Menu>().InsertAsync(newMenu);
+            bool isSuccessfully = await _unitOfWork.CommitAsync() > 0;
+            if (isSuccessfully) return newMenu.Id;
+            return Guid.Empty;
         }
 
         public async Task<HasBaseMenuResponse> CheckHasBaseMenuInBrand(Guid brandId)
         {
-	        if (brandId == Guid.Empty) throw new BadHttpRequestException(MessageConstant.Brand.EmptyBrandIdMessage);
-	        Brand brand = await _unitOfWork.GetRepository<Brand>()
-		        .SingleOrDefaultAsync(predicate: x => x.Id.Equals(brandId));
-	        if (brand == null) throw new BadHttpRequestException(MessageConstant.Brand.BrandNotFoundMessage);
-	        IEnumerable<Menu> menus = await _unitOfWork.GetRepository<Menu>()
-		        .GetListAsync(predicate: x => x.BrandId.Equals(brand.Id));
-	        if (menus == null)
-	        {
-				return new HasBaseMenuResponse()
-				{
-					HasBaseMenu = false
-				};
-	        }// brand đó chưa có menu nào
+            if (brandId == Guid.Empty) throw new BadHttpRequestException(MessageConstant.Brand.EmptyBrandIdMessage);
+            Brand brand = await _unitOfWork.GetRepository<Brand>()
+                .SingleOrDefaultAsync(predicate: x => x.Id.Equals(brandId));
+            if (brand == null) throw new BadHttpRequestException(MessageConstant.Brand.BrandNotFoundMessage);
+            IEnumerable<Menu> menus = await _unitOfWork.GetRepository<Menu>()
+                .GetListAsync(predicate: x => x.BrandId.Equals(brand.Id));
+            if (menus == null)
+            {
+                return new HasBaseMenuResponse()
+                {
+                    HasBaseMenu = false
+                };
+            }// brand đó chưa có menu nào
 
-	        if (menus.Any(x => x.Priority == 0))
-	        {
-		        return new HasBaseMenuResponse()
-		        {
-			        HasBaseMenu = true
-		        };
-	        }
-	        else
-	        {
-		        return new HasBaseMenuResponse()
-		        {
-			        HasBaseMenu = false
-		        };
-	        }
+            if (menus.Any(x => x.Priority == 0))
+            {
+                return new HasBaseMenuResponse()
+                {
+                    HasBaseMenu = true
+                };
+            }
+            else
+            {
+                return new HasBaseMenuResponse()
+                {
+                    HasBaseMenu = false
+                };
+            }
         }
 
-        public async Task<IPaginate<GetMenuDetailResponse>> GetMenus(Guid brandId,string? code, int page = 1, int size = 10)
+        public async Task<IPaginate<GetMenuDetailResponse>> GetMenus(Guid brandId, string? code, int page = 1, int size = 10)
         {
-	        if (brandId == Guid.Empty) throw new BadHttpRequestException(MessageConstant.Brand.BrandNotFoundMessage);
-	        Brand brand = await _unitOfWork.GetRepository<Brand>()
-		        .SingleOrDefaultAsync(predicate: x => x.Id.Equals(brandId));
-	        if (brand == null) throw new BadHttpRequestException(MessageConstant.Brand.BrandNotFoundMessage);
-	        code = code?.Trim();
-	        IPaginate<GetMenuDetailResponse> menusInBrand = await _unitOfWork.GetRepository<Menu>().GetPagingListAsync(
-				selector: x => new GetMenuDetailResponse(x.Id, x.Code, x.Priority, x.DateFilter, x.StartTime, x.EndTime, x.Status, x.CreatedBy, x.CreatedAt, x.UpdatedBy, x.UpdatedAt, x.MenuProducts.ToList(), x.MenuStores.ToList()),
-				predicate: string.IsNullOrEmpty(code) ? x => x.BrandId.Equals(brandId) : x => x.Code.ToLower().Equals(code) && x.BrandId.Equals(brandId),
-				include: x => x.Include(x => x.MenuStores).ThenInclude(x => x.Store).Include(x => x.MenuProducts).ThenInclude(x => x.Product).ThenInclude(x => x.Category),
-				page: page,
-				size: size
-	        );
-			return menusInBrand;
+            if (brandId == Guid.Empty) throw new BadHttpRequestException(MessageConstant.Brand.BrandNotFoundMessage);
+            Brand brand = await _unitOfWork.GetRepository<Brand>()
+                .SingleOrDefaultAsync(predicate: x => x.Id.Equals(brandId));
+            if (brand == null) throw new BadHttpRequestException(MessageConstant.Brand.BrandNotFoundMessage);
+            code = code?.Trim();
+            IPaginate<GetMenuDetailResponse> menusInBrand = await _unitOfWork.GetRepository<Menu>().GetPagingListAsync(
+                selector: x => new GetMenuDetailResponse(x.Id, x.Code, x.Priority, x.DateFilter, x.StartTime, x.EndTime, x.Status, x.CreatedBy, x.CreatedAt, x.UpdatedBy, x.UpdatedAt, x.MenuProducts.ToList(), x.MenuStores.ToList()),
+                predicate: string.IsNullOrEmpty(code) ? x => x.BrandId.Equals(brandId) : x => x.Code.ToLower().Equals(code) && x.BrandId.Equals(brandId),
+                include: x => x.Include(x => x.MenuStores).ThenInclude(x => x.Store).Include(x => x.MenuProducts).ThenInclude(x => x.Product).ThenInclude(x => x.Category),
+                page: page,
+                size: size
+            );
+            return menusInBrand;
         }
 
         public async Task<Guid> UpdateMenuProducts(Guid menuId, UpdateMenuProductsRequest updateMenuProductsRequest)
@@ -137,10 +138,10 @@ namespace Pos_System.API.Services.Implements
                     .Where(x => splittedProductIds.idsToAdd.Contains(x.ProductId)).ToList();
 
                 List<MenuProduct> prepareDataToInsert = new List<MenuProduct>();
-				productsToInsert.ForEach(x =>
-				{
-				Product referenceProductData = currentProductsInSystem.Find(y => y.Id.Equals(x.ProductId));
-				if (referenceProductData == null) throw new BadHttpRequestException(MessageConstant.Menu.ProductNotInBrandMessage + x.ProductId);
+                productsToInsert.ForEach(x =>
+                {
+                    Product referenceProductData = currentProductsInSystem.Find(y => y.Id.Equals(x.ProductId));
+                    if (referenceProductData == null) throw new BadHttpRequestException(MessageConstant.Menu.ProductNotInBrandMessage + x.ProductId);
                     prepareDataToInsert.Add(new MenuProduct
                     {
                         Id = Guid.NewGuid(),
@@ -158,8 +159,8 @@ namespace Pos_System.API.Services.Implements
                 await _unitOfWork.GetRepository<MenuProduct>().InsertRangeAsync(prepareDataToInsert);
             }
 
-			if(splittedProductIds.idsToKeep.Count > 0)
-			{
+            if (splittedProductIds.idsToKeep.Count > 0)
+            {
                 List<ProductToUpdate> productDataFromRequest = updateMenuProductsRequest.Products
                     .Where(x => splittedProductIds.idsToKeep.Contains(x.ProductId)).ToList();
                 List<MenuProduct> productsToUpdate = productsInMenu
@@ -168,11 +169,11 @@ namespace Pos_System.API.Services.Implements
                 productsToUpdate.ForEach(x =>
                 {
                     ProductToUpdate requestProductData = productDataFromRequest.Find(y => y.ProductId.Equals(x.ProductId));
-					if (requestProductData == null) return;
-					x.SellingPrice = requestProductData.SellingPrice;
-					x.DiscountPrice = requestProductData.DiscountPrice;
-					x.UpdatedBy = currentUserName;
-					x.UpdatedAt = currentTime;
+                    if (requestProductData == null) return;
+                    x.SellingPrice = requestProductData.SellingPrice;
+                    x.DiscountPrice = requestProductData.DiscountPrice;
+                    x.UpdatedBy = currentUserName;
+                    x.UpdatedAt = currentTime;
                 });
 
                 _unitOfWork.GetRepository<MenuProduct>().UpdateRange(productsToUpdate);
@@ -184,11 +185,31 @@ namespace Pos_System.API.Services.Implements
                     predicate: x => splittedProductIds.idsToRemove.Contains(x.ProductId)
                     && x.MenuId.Equals(menuId));
 
-				_unitOfWork.GetRepository<MenuProduct>().DeleteRangeAsync(prepareDataToRemove);
+                _unitOfWork.GetRepository<MenuProduct>().DeleteRangeAsync(prepareDataToRemove);
             }
 
-			await _unitOfWork.CommitAsync();
-			return menuId;
+            await _unitOfWork.CommitAsync();
+            return menuId;
+        }
+
+        public async Task<IPaginate<GetProductInMenuResponse>> GetProductInMenu(Guid menuId, string? productName, int page, int size)
+        {
+            Guid brandId = Guid.Parse(GetBrandIdFromJwt());
+            Brand brand = await _unitOfWork.GetRepository<Brand>()
+                .SingleOrDefaultAsync(predicate: x => x.Id.Equals(brandId));
+            if (brand == null) throw new BadHttpRequestException(MessageConstant.Brand.BrandNotFoundMessage);
+
+            IPaginate<GetProductInMenuResponse> productsInMenu = await _unitOfWork.GetRepository<MenuProduct>()
+                .GetPagingListAsync(
+                    selector: product => new GetProductInMenuResponse(product.ProductId, product.Product.Name, product.Product.Code, product.Product.PicUrl, product.SellingPrice,
+                         product.HistoricalPrice, product.DiscountPrice),
+                    predicate: product => string.IsNullOrEmpty(productName) ? product.MenuId.Equals(menuId) && product.Product.BrandId.Equals(brandId) : product.MenuId.Equals(menuId) && product.Product.BrandId.Equals(brandId) && product.Product.Name.Contains(productName),
+                    include: product => product.Include(product => product.Product),
+                    page: page,
+                    size: size
+                );
+            return productsInMenu;
+
         }
     }
 }
--- Pos-System/Services/Interfaces/IMenuService.cs
+++ Pos-System/Services/Interfaces/IMenuService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Drawing;
 using Pos_System.API.Payload.Request.Menus;
 using Pos_System.API.Payload.Response.Menus;
+using Pos_System.API.Payload.Response.Products;
 using Pos_System.Domain.Paginate;
 
 namespace Pos_System.API.Services.Interfaces
@@ -12,5 +13,6 @@ namespace Pos_System.API.Services.Interfaces
         public Task<HasBaseMenuResponse> CheckHasBaseMenuInBrand(Guid brandId);
         public Task<IPaginate<GetMenuDetailResponse>> GetMenus(Guid brandId, string? code, int page = 1, int size = 10);
         public Task<Guid> UpdateMenuProducts(Guid menuId, UpdateMenuProductsRequest updateMenuProductsRequest);
+        public Task<IPaginate<GetProductInMenuResponse>> GetProductInMenu(Guid menuId, string? productName, int page, int size);
     }
 }
