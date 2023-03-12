5047f516cdc4b90e88ba7e2f10d1b6faae5bc64f
quandangdong <dongquan1212@gmail.com>
2023-03-12 21:49:46 +0700

    Fix error message

--- Pos-System/Payload/Request/Stores/CreateNewStoreRequest.cs
+++ Pos-System/Payload/Request/Stores/CreateNewStoreRequest.cs
@@ -9,7 +9,7 @@ namespace Pos_System.API.Payload.Request.Stores
         [MaxLength(50, ErrorMessage = "Store's name max length is 50 characters")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Short name is required")]
-        [MaxLength(30, ErrorMessage = "Store's short name max length is 50 characters")]
+        [MaxLength(30, ErrorMessage = "Store's short name max length is 30 characters")]
         public string? ShortName { get; set; }
         [EmailAddress(ErrorMessage = "Invalid Email")]
         [Required(ErrorMessage = "Store's email is required")]
