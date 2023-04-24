c9009957d30f7f14c96a7210b4ee47172cd1ecc8
Thanh Phan <megastar24577@gmail.com>
2023-04-24 15:59:30 +0700

    [NO JIRA] Fixed duplicated session when create or update

--- Pos-System/Constants/MessageConstant.cs
+++ Pos-System/Constants/MessageConstant.cs
@@ -112,6 +112,9 @@ public static class MessageConstant
     {
         public const string EmptySessionIdMessage = "Id của session không hợp lệ";
         public const string SessionNotFoundMessage = "Session không tồn tại trong store";
+        public const string CreateNewSessionInvalidStartDate = "Session startDate bị trùng với session: ";
+        public const string CreateNewSessionInvalidEndDate = "Session endDate bị trùng với session: ";
+        public const string AlreadySessionAvailableInTimeGap = "Thời gian của session bạn đang tạo hoặc cập nhật hiện đã có một vài session vui lòng thay đổi thời gian phù hợp";
     }
 
     public static class GroupProduct
